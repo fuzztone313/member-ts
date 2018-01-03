514d7899fa4a633a791e661f6532e4a9aa2ae82d
fuzztone313 <normanramadhan3@gmail.com>
2018-01-03 10:46:23 +0700

    Update
    Updating

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,12 +1,29 @@
 $(function(){
-    navTabs();
+    navMa();
+    navMp();
+    froala();
 });
 
 
-function navTabs() {
+function navMa() {
     $(".tabs-box").on("click", "li", function(){
         var tabsId = $(this).attr("id");
         $(this).addClass("tab-active").siblings().removeClass("tab-active");
         $("#" + tabsId + "-content-box").addClass("tab-active").siblings().removeClass("tab-active");
+    })
+}
+
+function navMp() {
+    $(".tabs-box-mp").on("click", "li", function(){
+        var tabsId = $(this).attr("id");
+        $(this).addClass("tab-active").siblings().removeClass("tab-active");
+        $("#" + tabsId + "-content-box").addClass("tab-active").siblings().removeClass("tab-active");
+    })
+}
+
+function froala() {
+     $('div#eg-gray-theme').froalaEditor({
+      theme: 'gray',
+      zIndex: 2001
     })
 }
